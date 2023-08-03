458a198f27797d3d3969d15eab1837a387897691
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-03 12:00:45 -0600

    Add relation between entities

--- CleanArchitecture.Domain/Director.cs
+++ CleanArchitecture.Domain/Director.cs
@@ -1,16 +1,22 @@
 ﻿using CleanArchitecture.Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CleanArchitecture.Domain
 {
     public class Director : BaseDomainModel
     {
+        public Director()
+        {
+        }
+
         public string? Nombre { get; set; }
 
         public string? Apellido { get; set; }
 
-        public int VideoId { get; set; }
+        [NotMapped]
+        public string? NombreCompleto => $"{Nombre} {Apellido}";
 
-        public virtual Video? Video { get; set; }
+        public virtual ICollection<Video> Videos { get; set; }
 
     }
 }
